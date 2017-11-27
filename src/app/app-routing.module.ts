4c98906cf8de7690ef1f81200c279364c5830f15
Brock Billings <brock_billings@me.com>
2017-11-27 17:17:04 -0600

    Created dashboard layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 //import { AuthGuard } from "./users/auth-guard.service";
-// import { DashboardComponent } from "./dashboard/dashboard.component";
-// import { Screen01Component } from "./dashboard/screen-01.component";
-// import { Screen02Component } from "./dashboard/screen-02.component";
 
 const ROUTES = [
   {
